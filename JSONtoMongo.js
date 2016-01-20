f0ce9948edf6e6c2f8941ca285663c428111ea9e
Chase Farmer <chasefarmer2808@ufl.edu>
2016-01-19 23:26:02 -0500

    finished query functions

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,51 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    Factory = require('./listings.json'),
     config = require('./config');
 
 /* Connect to your database */
-
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+mongoose.connect(config.db.uri);
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
 
+var entries = Factory.entries;
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
- */
+for (var i = 0; i < entries.length; i++) {
+  var newListing;
+
+  if (entries[i].hasOwnProperty('coordinates')) {
+    newListing = new Listing({
+      code: entries[i].code,
+      name: entries[i].name,
+      coordinates: {
+        latitude: entries[i].coordinates.latitude,
+        longitude: entries[i].coordinates.longitude
+      },
+      address: entries[i].address
+    });
+  } else {
+    newListing = new Listing({
+      code: entries[i].code,
+      name: entries[i].name
+    });
+  }
+
+  /*
+    Once you've written + run the script, check out your MongoLab database to ensure that
+    it saved everything correctly.
+   */
+  newListing.save(function(err) {
+    if (err) throw err;
+
+    console.log('New Listing Created');
+  })
+}
--- queries.js
+++ queries.js
@@ -1,28 +1,62 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
+
+mongoose.connect(config.db.uri);
 /* Fill out these functions using Mongoose queries*/
 
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+
+   Listing.find({name: 'Library West'}, function(err, listing) {
+     if (err) throw err;
+
+     console.log(listing);
+   });
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+
+   Listing.findOneAndRemove({code: 'CABL'}, function(err) {
+     if (err) throw err;
+
+     console.log('Listing deleted!');
+   })
 };
 var updatePhelpsMemorial = function() {
   /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
+
+   Listing.find({code: 'PHL'}, function(err, listing) {
+     if (err) throw err;
+
+     listing.address = '100 Phelps Lab, Museum Road, Gainesville FL, 32611, United States';
+     listing.latitude = 29.644548;
+     listing.longitude = -82.348819;
+
+     console.log(listing);
+   })
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+
+   Listing.find({}, function(err, listings) {
+     if (err) throw err;
+
+     console.log(listings);
+   })
 };
 
 findLibraryWest();
